3c6a2f775f2272c741cb8383173127fe38be2993
HAWNBINYOO <dngh0825@naver.com>
2022-03-20 13:17:40 +0900

    [fix] header 수정 content 추가

--- src/Components/NoticeBoard/NoticeBoard.tsx
+++ src/Components/NoticeBoard/NoticeBoard.tsx
@@ -1,10 +1,13 @@
 import * as S from "./Style";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const NoticeBoard = () => {
   return (
     <>
-      <S.NoticeBoard />
+      <S.NoticeBoard>
+        <p>게시판</p>
+        <S.Centent></S.Centent>
+      </S.NoticeBoard>
     </>
   );
 };
--- src/Components/NoticeBoard/Style.ts
+++ src/Components/NoticeBoard/Style.ts
@@ -1,3 +1,16 @@
 import styled from "styled-components";
 
-export const NoticeBoard = styled.div``;
+export const NoticeBoard = styled.div`
+  display: flex;
+
+  p {
+    font-size: 2rem;
+  }
+
+  width: 90%;
+  height: 80vh;
+  margin: 100px auto 0;
+  background-color: black;
+`;
+
+export const Centent = styled.div``;

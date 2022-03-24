ff94881e34673572f5b412a008e6e9d97d528a8c
kangkong <kang3530554@gmail.com>
2022-03-24 14:09:58 +0900

    main 브랜치에 push해버려서 개고생 중...

--- src/Components/SignUp/Style.ts
+++ src/Components/SignUp/Style.ts
@@ -1,61 +1,61 @@
 import styled from "styled-components";
+import apple from "../../Assets/apple.png"
 
 export const BackgroundContainer = styled.div`
     width: 100vw;
     height: 100vh;
-    display: flex;
     align-items: center;
-    background-color: #E9ECEF;
+    background-image: url(${apple});
+    background-repeat : no-repeat;
+    background-position: left bottom;
+    background-size: 81.6vh;
 `
 
 export const BackgroundWrapper = styled.div`
     width: 35vw;
     min-width: 300px;
     height: 70vh;
-    margin: 0 auto;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    border: 1px solid black;
-    border-radius: 50px;
     gap: 20px;
     background-color: white;
+    top: 10vh;
+    right: 10vw;
+    margin-left:auto;
+    position: relative;
 `
 
 export const UserWrapper = styled.div`
     width: 100%;
-    height: 100px;
+    height: auto;
 `
 
 export const UserInput = styled.input`
-    background-color: rgba(255, 230, 80, 0.15);
-    width: 99%;
-    height: 100%;
-    border: 0;
+    width: calc(100% - 2px);
+    height: 100px;
+    border: 1px solid black;
+    border-radius: 10px;
     padding: 0;
-    padding-left: 1%;
     font-size: 20px;
     text-align: center;
 `
 
 export const UserChoiceContainer = styled.div`
     width: 100%;
-    display: flex;
-    justify-content: space-around;
-`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    justify-items: center;
+    margin-top: 10px;
 
-export const UserChoiceWrapper = styled.div`
-    width: auto;
-    display: flex;
-    flex-direction: column;
 `
 
 export const UserSumbit = styled.button`
     width: 30%;
     height: 10%;
-    border: 1px solid rgb(255, 230, 80);
+    border: 0;
     border-radius: 100px;
     position: relative;
-    background-color: rgba(255, 230, 80, 0.15);
+    background-color: #FFED48;
 `

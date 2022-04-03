0aec44c77c7ff46bbc8a25a9133424104bad38a7
kangkong <kang3530554@gmail.com>
2022-04-03 23:04:26 +0900

    Start making profile feature

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,16 +1,40 @@
+import axios from "axios";
+import { profile } from "console";
+import { useEffect, useState } from "react";
 import * as S from "./Style";
+
+type UserType = {
+    memberIdx: number;
+    name: string;
+    profile: string;
+    theme: string;
+    postings: [];
+};
 
 export default function Profile() {
 
-    return(
+    const [profle, setProfile] = useState<UserType>();
+    const [memberIdx, name, profile, theme, postings] = profile;
+
+    useEffect(() => {
+        axios.get('/v1/member/{memberIdx}')
+            .then(({data}:any) => {
+                setProfile(data);
+            })
+            .catch((error:any) => {
+                throw new Error(error);
+            })
+    }, [])
+
+    return (
         <>
             <S.ProfileLayout>
                 <S.Container>
-                    <S.ProfilePicture src={require("../../Assets/pig.png")}/>
+                    <S.ProfilePicture src={require("../../Assets/pig.png")} />
                 </S.Container>
                 <S.Container>
                     <S.UserContainer>
-                        <S.UserWrapper>이름 : 1일</S.UserWrapper>
+                        <S.UserWrapper>{}</S.UserWrapper>
                         <S.UserWrapper>몸무게 : 100kg</S.UserWrapper>
                         <S.UserWrapper>목표 몸무게 : 80kg</S.UserWrapper>
                         <S.UserWrapper>식단 : 단백질 위주</S.UserWrapper>

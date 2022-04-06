8c9d9c7fb77f67ff43313c6047de6674e4af8f50
HAWNBINYOO <dngh0825@naver.com>
2022-04-06 21:07:55 +0900

    4/6/21

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import { GlobalStyle } from "./Style/GrobalStyle";
 import NoticePage from "./Page/NoticePage";
 import SignUp from "./Components/SignUp/SignUp";
 import LoginPage from "./Page/LoginPage/LoginPage";
-import Start from "./Components/Start/Start";
 import ProfilePage from "./Page/ProfilePage/ProfilePage";
+import StartPage from "./Page/StartPage";
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
-        <Route path="/" element={<Start />} />
+        <Route path="/" element={<StartPage />} />
         <Route path="/notice" element={<NoticePage />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<LoginPage />} />

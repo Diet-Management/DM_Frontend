cc1d4876cd5c02dac937c03087ed6521b31f73d0
kangkong <kang3530554@gmail.com>
2022-04-06 19:48:05 +0900

    Code cleanup

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,27 +1,25 @@
 import axios from "axios";
-import { profile } from "console";
 import { useEffect, useState } from "react";
 import * as S from "./Style";
 
-type UserType = {
+type profiletype = {
     memberIdx: number;
     name: string;
     profile: string;
     theme: string;
     postings: [];
-};
+}
 
 export default function Profile() {
 
-    const [profle, setProfile] = useState<UserType>();
-    const [memberIdx, name, profile, theme, postings] = profile;
+    const [profile, setProfile] = useState<profiletype>();
 
     useEffect(() => {
         axios.get('/v1/member/{memberIdx}')
-            .then(({data}:any) => {
+            .then(({ data }: any) => {
                 setProfile(data);
             })
-            .catch((error:any) => {
+            .catch((error: any) => {
                 throw new Error(error);
             })
     }, [])
@@ -34,7 +32,7 @@ export default function Profile() {
                 </S.Container>
                 <S.Container>
                     <S.UserContainer>
-                        <S.UserWrapper>{}</S.UserWrapper>
+                        <S.UserWrapper>이름 : {profile?.name}</S.UserWrapper>
                         <S.UserWrapper>몸무게 : 100kg</S.UserWrapper>
                         <S.UserWrapper>목표 몸무게 : 80kg</S.UserWrapper>
                         <S.UserWrapper>식단 : 단백질 위주</S.UserWrapper>
